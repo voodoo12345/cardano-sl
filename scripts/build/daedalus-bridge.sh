f02919463e1dbb4a72621cfc9c6e8d76a4d9b9a3
Anupam Jain <ajnsit@gmail.com>
2017-06-22 19:08:35 +0530

    Pass through all arguments from daedalus-bridge build script to cardano-sl build script
    This allows is to pass arguments like --prod and --ram to
    ./scripts/build/daedalus-bridge.sh

--- scripts/build/daedalus-bridge.sh
+++ scripts/build/daedalus-bridge.sh
@@ -9,7 +9,7 @@ echo "Building Daedalus Bridge..."
 
 # We have to build Cardano SL because there's a code in Haskell
 # codebase that needs to be recompiled for Bridge building.
-./scripts/build/cardano-sl.sh
+./scripts/build/cardano-sl.sh "$@"
 
 echo "2. Generating types for Bridge..."
 stack exec -- cardano-wallet-hs2purs
